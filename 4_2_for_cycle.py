322c13bfb571afb52f34c58130643dd54ba95fa3
bszeidl <szeidl.balazs@hotmail.com>
2023-05-09 22:25:24 +0200

    For cycle task ready.

---
+++ 4_2_for_cycle.py
@@ -0,0 +1,11 @@
+favorite_team = ['Armstrong, BJ', 'Cartwright, Bill (C)', 'Grant, Horace', 'Hodges, Craig', 
+                 'Hopson, Dennis', 'Jordan, Michael (C)', 'King, Stacey', 'Levingston, Cliff',
+                 '	Paxson, John', 'Perdue, Will', 'Pippen, Scottie', 'Williams, Scott']
+
+
+def print_player_names(team):
+    for player in team:
+        print(player)
+
+print_player_names(favorite_team)
+
