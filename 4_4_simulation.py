8fb102591621a17130e34a4d3e75faf30a0a929a
bszeidl <szeidl.balazs@hotmail.com>
2023-05-15 23:30:14 +0200

    Lesson completed.

---
+++ 4_4_simulation.py
@@ -0,0 +1,9 @@
+def quarter_counter():
+    for quarter in range(1,5):
+        for minute in range(1,13):
+            print(str(quarter) + '. negyed ' + str(minute) + '. perc.')     
+           
+
+quarter_counter()
+
+
